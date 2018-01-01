b27b4467beb049eacee3717e2c261b69846ddfee
Tavi Nathanson <tavi.nathanson@gmail.com>
2018-01-01 14:41:33 -0500

    Add initial rsvp method

--- rsvp.py
+++ rsvp.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from flask.json import jsonify
+from six.moves.urllib.parse import unquote_plus
 
 app = Flask(__name__)
 
 @app.route('/')
-def picks():
+def index():
     return 'rebeccaandtavi.com RSVP!'
+
+@app.route('/rsvp', methods=['POST'])
+def rsvp():
+    data = request_dict(request.get_data())
+    email = unquote_plus(data["email"])
+    return email
